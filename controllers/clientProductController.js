8e57270863613204f5952b4a360c2c426520c4e7
Егор Богатырёв <egorbogatyrev@MacBook-Pro-Egor.local>
2025-03-28 01:26:39 +0500

    fix

--- controllers/clientProductController.js
+++ controllers/clientProductController.js
@@ -1,29 +1,36 @@
 const ApiError = require('../error/ApiError')
 const { s3 } = require('../db');
 const sharp = require('sharp');
-const {Category, Product} = require('../models/models')
+const { Category, Product } = require('../models/models')
 const Op = require('sequelize').Op;
 
 class ProductController {
     async fetch(req, res, next) {
         try {
-            let {page, limit, categoryId} = req.query
+            let { page, limit, categoryId } = req.query
             page = page || 1
             limit = limit || 100
             let offset = page * limit - limit
 
-            const categories = await Category.findAll({where: {isDeleted: false,
-                [Op.or]: [{id: categoryId}, {parentId: categoryId}]
-            }})
+            let categories = []
 
-            const categoryIds = categories.map((category) => category.id)
-            
+            if (categoryId) {
+                categories = await Category.findAll({
+                    where: {
+                        isDeleted: false,
+                        [Op.or]: [{ id: categoryId }, { parentId: categoryId }]
+                    }
+                })
+            }
+
+            const categoryIds = categories?.map((category) => category.id) || []
+
             const products = await Product.findAndCountAll({
                 where: {
                     isDeleted: false,
-                    ...(categoryId ? {categoryId: {[Op.in]: categoryIds}} : {}),
+                    ...(categoryId ? { categoryId: { [Op.in]: categoryIds } } : {}),
                 },
-                limit, offset, order: [['name', 'ASC']], include: [{model: Category}]
+                limit, offset, order: [['name', 'ASC']], include: [{ model: Category }]
             })
 
             return res.json(products)
@@ -35,16 +42,16 @@ class ProductController {
     async fetchOne(req, res, next) {
         try {
             const { link } = req.params;
-            const product = await Product.findOne({where: {link}, include: [{model: Category}]})
+            const product = await Product.findOne({ where: { link }, include: [{ model: Category }] })
             return res.json(product)
         } catch (e) {
             next(ApiError.badRequest(e.message))
         }
     }
-    
+
     async fetchByCategory(req, res, next) {
         try {
-            const categories = await Category.findAll({include: [{model: Product, required: true}]})
+            const categories = await Category.findAll({ include: [{ model: Product, required: true }] })
             return res.json(categories)
         } catch (e) {
             next(ApiError.badRequest(e.message))
@@ -61,47 +68,47 @@ class ProductController {
             } = JSON.parse(req.body.data);
 
             let filesPromises = []
-    
-            if(files && files.length > 0){
+
+            if (files && files.length > 0) {
                 filesPromises = files.map(async (file) => {
                     if (file) {
                         // Загрузка оригинального изображения
                         const upload = await s3.Upload({ buffer: file.data }, '/products/');
                         const imageUrl = upload.Key;
-    
+
                         const previewBuffer = await sharp(file.data)
                             .resize(24, 24)
                             .toBuffer();
-            
+
                         // Загрузка миниатюры на S3
                         const previewUpload = await s3.Upload({ buffer: previewBuffer }, '/products/previews/');
                         const previewUrl = previewUpload.Key;
-    
-                        return {imageUrl, previewUrl}
+
+                        return { imageUrl, previewUrl }
                     }
                 });
             }
 
             const filesData = await Promise.all(filesPromises);
-    
+
             const product = await Product.create({
-                name, 
-                description, 
-                link, 
-                price, 
-                old_price, 
-                categoryId, 
-                about, 
-                weight, 
-                variation, 
-                processing, 
-                fermentation, 
-                region, 
-                farmer, 
+                name,
+                description,
+                link,
+                price,
+                old_price,
+                categoryId,
+                about,
+                weight,
+                variation,
+                processing,
+                fermentation,
+                region,
+                farmer,
                 keyDescriptor,
                 images: filesData
             });
-    
+
             return res.json(product)
         } catch (e) {
             console.log(e)
@@ -112,7 +119,7 @@ class ProductController {
     async update(req, res, next) {
         try {
             const files = req.files?.files;
-            const {id} = req.query;
+            const { id } = req.query;
             const {
                 name, description, link, price, old_price, categoryId,
                 about, weight, variation, processing, fermentation,
@@ -120,47 +127,47 @@ class ProductController {
             } = JSON.parse(req.body.data);
 
             let filesPromises = []
-    
-            if(files && files.length > 0){
+
+            if (files && files.length > 0) {
                 filesPromises = files.map(async (file) => {
                     if (file) {
                         // Загрузка оригинального изображения
                         const upload = await s3.Upload({ buffer: file.data }, '/products/');
                         const imageUrl = upload.Key;
-    
+
                         const previewBuffer = await sharp(file.data)
                             .resize(24, 24)
                             .toBuffer();
-            
+
                         // Загрузка миниатюры на S3
                         const previewUpload = await s3.Upload({ buffer: previewBuffer }, '/products/previews/');
                         const previewUrl = previewUpload.Key;
-    
-                        return {imageUrl, previewUrl}
+
+                        return { imageUrl, previewUrl }
                     }
                 });
             }
 
             const filesData = await Promise.all(filesPromises);
-    
+
             const product = await Product.update({
-                name, 
-                description, 
-                link, 
-                price, 
-                old_price, 
-                categoryId, 
-                about, 
-                weight, 
-                variation, 
-                processing, 
-                fermentation, 
-                region, 
-                farmer, 
+                name,
+                description,
+                link,
+                price,
+                old_price,
+                categoryId,
+                about,
+                weight,
+                variation,
+                processing,
+                fermentation,
+                region,
+                farmer,
                 keyDescriptor,
                 images: filesData
-            }, {where: {id}});
-    
+            }, { where: { id } });
+
             return res.json(product)
         } catch (e) {
             console.log(e)
@@ -168,10 +175,10 @@ class ProductController {
         }
     }
 
-    async delete (req, res, next) {
+    async delete(req, res, next) {
         try {
-            let {id} = req.query;
-            await Product.update({isDeleted: true}, {where: {id}})
+            let { id } = req.query;
+            await Product.update({ isDeleted: true }, { where: { id } })
             return res.json("Deleted successfully");
         } catch (e) {
             next(ApiError.badRequest(e.message))
