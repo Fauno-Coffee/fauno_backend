5ca0357d9e34b51a643a7013a4c6b336e9549e87
Егор Богатырёв <egorbogatyrev@MacBook-Pro-Egor.local>
2025-04-29 14:08:51 +0500

    fix cors

--- utils/server.js
+++ utils/server.js
@@ -4,9 +4,11 @@ const router = require('../routes/index')
 const errorHandler = require('../middleware/ErrorHandlingMiddleware')
 const fileUpload = require('express-fileupload')
 
-module.exports = function createServer(){
+module.exports = function createServer() {
     const app = express()
-    app.use(cors());
+    app.use(cors({
+        origin: ['https://admin.docly.ru', 'https://docly.ru', 'http://localhost:3000']
+    }));
     app.use(fileUpload({}))
     app.use('/static', express.static(__dirname + '/public'));
     app.use(express.static('files'));
